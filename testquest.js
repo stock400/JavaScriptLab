5f56ff19feb96635cb89fb28938e7a382dbce24f
stock400 <stock400@morris.umn.edu>
2014-01-31 15:22:09 -0600

    Trying to figure out makingSave closure

---
+++ testquest.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+
+
+describe('Saving', function() {
+    describe('test 1', function() {
+        it('should save the state of a textbox into an array', function(){
+            assert.equal(toSave(SOMETHING), truth);
+        })
+    });
+});
