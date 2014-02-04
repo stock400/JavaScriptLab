a0c33e84b52ed63bf7ac805a395d94047aab2368
stock400 <stock400@morris.umn.edu>
2014-02-03 21:04:49 -0600

    Save Tests Passed

--- testquest.js
+++ testquest.js
@@ -2,22 +2,48 @@ var assert = require("assert");
 var makeSave = require('./javaforpage').makeSave;
 
 var testSave = makeSave("save1");
-var testArray = ["save1"];
+
 describe('Saving', function() {
-    describe('test 1', function() {
-        it('should save the initial value into an array', function(){
-            assert.equal(testSave.toString(), testArray);
+    describe('Save test 1', function() {
+        it('should save the initial value into an array and return it', function(){
+            assert.equal(testSave.save("save2"), "save2");
         })
     });
-    testSave.save("save2");
-    testSave.save("save3");
-    testSave.save("save4");
-    testSave.save("save5");
-    testArray.push("save2", "save3", "save4", "save5");
 
-    describe('test 2', function() {
+    describe('Save test 2', function() {
         it('should print full array of saved states', function() {
-            assert.equal(testSave.toString(), testArray);
+            testSave.save("save3");
+            testSave.save("save4");
+            testSave.save("save5");
+            assert.equal(testSave.save("save6"), "save6");
         })
-    })
+    });
+
+    describe('Undo test 1', function() {
+        it('should undo textarea state and return the undone state', function() {
+            assert.equal(testSave.undo(), "save5");
+        })
+    });
+
+    describe('Undo test 2', function() {
+        it('should undo textarea state and return the undone state', function() {
+            assert.equal(testSave.undo(), "save4");
+        })
+    });
+
+    describe('Redo test 1', function() {
+        it('should undo textarea state and return the undone state', function() {
+            assert.equal(testSave.redo(), "save5");
+        })
+    });
+
+    describe('Redo test 2', function() {
+        it('should undo textarea state and return the undone state', function() {
+            assert.equal(testSave.redo(), "save6");
+        })
+    });
+
+
+
+
 });
