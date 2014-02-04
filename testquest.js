fb531f38c2ec9edf533cd3a9bd575759d3c103d7
stock400 <stock400@morris.umn.edu>
2014-02-04 00:08:57 -0600

    charizardCount tests added

--- testquest.js
+++ testquest.js
@@ -1,17 +1,18 @@
 var assert = require("assert");
 var makeSave = require('./javaforpage').makeSave;
+var charizardCount = require('./javaforpage').charizardCount;
 
 var testSave = makeSave("save1");
 
 describe('Saving', function() {
     describe('Save test 1', function() {
-        it('should save the initial value into an array and return it', function(){
+        it('should save the textarea value into an array and return the saved value', function(){
             assert.equal(testSave.save("save2"), "save2");
         })
     });
 
     describe('Save test 2', function() {
-        it('should print full array of saved states', function() {
+        it('should save the textarea value into an array and return the saved value', function() {
             testSave.save("save3");
             testSave.save("save4");
             testSave.save("save5");
@@ -32,18 +33,32 @@ describe('Saving', function() {
     });
 
     describe('Redo test 1', function() {
-        it('should undo textarea state and return the undone state', function() {
+        it('should redo textarea state and return the redone state', function() {
             assert.equal(testSave.redo(), "save5");
         })
     });
 
     describe('Redo test 2', function() {
-        it('should undo textarea state and return the undone state', function() {
+        it('should redo textarea state and return the redone state', function() {
             assert.equal(testSave.redo(), "save6");
         })
     });
 
+});
 
+describe('Character Count', function() {
+    describe('Count test 1', function() {
+        it('should count the length of text', function(){
+            assert.equal(charizardCount("This will be 16."), 16);
+        })
+    });
+
+    describe('Count test 2', function() {
+        it('should count the length of text', function(){
+            assert.equal(charizardCount("123456"), 6);
+        })
+    });
 
 
 });
+
